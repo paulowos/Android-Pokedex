92e7eb94b8d1d37304b9460293c825455b93e7ba
Paulo Willian <paulowos@gmail.com>
2024-07-29 11:50:24 -0300

    Navigation and Hilt

--- app/src/main/java/com/example/androidpokedex/MainActivity.kt
+++ app/src/main/java/com/example/androidpokedex/MainActivity.kt
@@ -4,14 +4,54 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.androidpokedex.ui.theme.AndroidPokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AndroidPokedexTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "pokemon_list_screen",
+                ) {
+                    composable("pokemon_list_screen") {
+                    }
+                    composable(
+                        "pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                        arguments =
+                            listOf(
+                                navArgument("dominantColor") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("pokemonName") {
+                                    type = NavType.StringType
+                                },
+                            ),
+                    ) {
+                        val dominantColor =
+                            remember {
+                                val color = it.arguments?.getInt("dominantColor")
+                                color?.let { Color(it) } ?: Color.White
+                            }
+
+                        val pokemonName =
+                            remember {
+                                it.arguments?.getString("pokemonName")
+                            }
+                    }
+                }
             }
         }
     }
